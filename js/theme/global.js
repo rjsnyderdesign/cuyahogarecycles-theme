040665c8cd478318a35ee8ce7bbe17ec1634518d
Ken Sugiura <ksugiura@traitstudio.com>
2016-04-19 10:46:28 -0400

    Staff tile touch events

--- js/theme/global.js
+++ js/theme/global.js
@@ -736,9 +736,24 @@
         //
 
         ( function () {
-            $( '.tile-grid.tile-grid-staff > .grid-cell a' ).on( 'touchstart', function () {
-                $( this ).focus();
+
+            var $staffTiles = $( '.tile-grid.tile-grid-staff > .grid-cell a' );
+
+            $staffTiles.data( 'touch-count', '0' );
+
+            $staffTiles.on( 'touchstart', function ( e ) {
+                var $this = $( this );
+                if ( $this.data( 'touch-count' ) === '0' ) {
+                    $this.focus().data( 'touch-count', '1' );
+                    e.preventDefault();
+                    return false;
+                }
+                else if ( $this.data( 'touch-count' ) === '1' ) {
+                    $this.data( 'touch-count', '2' );
+                    return true;
+                }
             } );
+
         } )();
 
         //
